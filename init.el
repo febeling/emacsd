0bf28542195d9f12b6e4be9dee704b94bf44a70e
Caspar Florian Ebeling <florian.ebeling@gmail.com>
2006-07-31 08:13:04 +0000

    ignore errors in load of ruby and slime extensions.

--- init.el
+++ init.el
@@ -1,6 +1,25 @@
-(setq make-backup-file nil)
+(setq make-backup-files nil)
+(setq default-case-fold-search t)
+(setq auto-compression-mode t)
+
+(global-font-lock-mode)
+(show-paren-mode)
+
+
+(add-hook 'text-mode-hook 'turn-on-auto-fill)
+
+(setq load-path (cons "~/.emacs.d" load-path))
+(require 'ruby-mode)
+
+
+(defun insert-date ()
+  "Insert a date at point"
+  (interactive)
+  (insert (format-time-string "%d.%m.%y")))
+
 
 (setq default-frame-alist '((top . 1) (left . 1) (width . 120) (height . 52)))
+
 
 (if (fboundp 'scroll-bar-mode) (scroll-bar-mode -1)) 
 (if (fboundp 'tool-bar-mode) (tool-bar-mode -1)) 
@@ -13,20 +32,23 @@
 
 
 (fset 'to-java-string
-   [?\C-a ?" ?\C-e ?" ?  ?+ down])
+      [?\C-a ?" ?\C-e ?" ?  ?+ down])
 (global-set-key [f6] 'to-java-string)
 (fset 'purge-line
-   [?\C-a ?\C-  ?\C-n ?\C-c ?\C-k])
+      [?\C-a ?\C-  ?\C-n ?\C-c ?\C-k])
 (global-set-key [\C-K] 'purge-line)
+(fset 'mark-as-done
+      [?\C-e ?  ?( ?\M-x ?i ?n ?s ?e ?r ?t ?- ?d ?a ?t ?e return ?) ?\C-a ?\C-k ?\C-k ?\C-s ?D ?O ?N ?E ?\C-m return ?\C-y ?\C-k ?\C-r ?T ?O ?D ?O ?\C-m ?\C-n])
+(global-set-key [f2] 'mark-as-done)
 
 
 (global-set-key [f5] 'call-last-kbd-macro)
 (global-set-key [f3] 'edit-last-kbd-macro)
 
-
+;; free strokes
 ;; C-# as new/custom duplicate-line keybinding  
 ;; M-p another free keybinding
-
+;; C-. 
 
 (global-set-key "\C-c\C-m" 'execute-extended-command)
 ;(global-set-key "\C-x\C-m" 'execute-extended-command)
@@ -42,8 +64,11 @@
 (setq inferior-lisp-program "cmucl") 
 ;(setq inferior-lisp-program "clisp -K full") 
 ;(setq inferior-lisp-program "sbcl")
-(require 'slime)
-(slime-setup)
+(condition-case ()
+    (progn
+      (require 'slime)
+      (slime-setup))
+  (error (message "  slime not present - error on loading")))
 
 ;; from slime/HACKING:
 (defun show-outline-structure ()
@@ -60,7 +85,9 @@
       '(try-complete-abbrev
 	try-complete-file-name
 	try-expand-dabbrev))
-(require 'rails)
+(condition-case ()
+    (require 'rails)
+  (error (message "  rails not present - error on loading")))
 
 
 
