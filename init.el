72660b2947d194fbe00ead4a4aa0b6cff83a951e
Caspar Florian Ebeling <florian.ebeling@gmail.com>
2007-05-09 11:57:52 +0000

    cperl item snippet

--- init.el
+++ init.el
@@ -173,6 +173,12 @@ $.
 
   end"))
 
+(snippet-with-abbrev-table 
+ 'cperl-mode-abbrev-table
+ ("item" . "=item1$.
+
+=cut"))
+
 
 (require 'slime)
 ;;; Optionally, specify the lisp program to use. Default is "lisp"
@@ -209,3 +215,15 @@ $.
     (require 'rails)
   (error (message "  rails not present - error on loading")))
 
+(custom-set-variables
+  ;; custom-set-variables was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
+ '(safe-local-variable-values (quote ((cperl-indent-level . 4) (cperl-indent-level . 2)))))
+(custom-set-faces
+  ;; custom-set-faces was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
+ )
