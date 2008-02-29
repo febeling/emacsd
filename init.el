5ea16e5dbc304f39d524de2a6235ce0993266901
Caspar Florian Ebeling <florian.ebeling@gmail.com>
2008-02-29 08:57:22 +0000

    speedbar customized to show unknown files.

--- init.el
+++ init.el
@@ -285,7 +285,8 @@ $.
   ;; If you edit it by hand, you could mess it up, so be careful.
   ;; Your init file should contain only one such instance.
   ;; If there is more than one, they won't work right.
- '(safe-local-variable-values (quote ((cperl-indent-level . 4) (cperl-indent-level . 2)))))
+ '(safe-local-variable-values (quote ((cperl-indent-level . 4) (cperl-indent-level . 2))))
+ '(speedbar-show-unknown-files t))
 (custom-set-faces
   ;; custom-set-faces was added by Custom.
   ;; If you edit it by hand, you could mess it up, so be careful.
