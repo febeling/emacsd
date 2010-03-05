0ecc8d125d0194f8b7dcbd2ed7f0e2718b2fc20f
Caspar Florian Ebeling <florian.ebeling@gmail.com>
2010-03-05 21:33:49 +0100

    init.el: set cursor color for v23; remove from v23 backported function.

--- init.el
+++ init.el
@@ -6,6 +6,8 @@
 
 ;;; Todos:
 
+(set-cursor-color "gray46")
+
 (add-to-list 'load-path "~/.emacs.d")
 (add-to-list 'load-path "~/.emacs.d/ruby-test-mode")
 
@@ -154,54 +156,6 @@ after a line to extend them."
                          slime-lisp-implementations)))
   (save-window-excursion
     (slime)))
-
-;; referenced from Phil Hagelberg's blog, only needed for Emacs 22, 23
-;; contains it, it seems.
-
-;; Port this functionality back from Emacs 23 since it's Really Useful
-
-(unless (functionp 'locate-dominating-file)
-  (defun locate-dominating-file (file name)
-    "Look up the directory hierarchy from FILE for a file named NAME.
-Stop at the first parent directory containing a file NAME,
-and return the directory.  Return nil if not found."
-    ;; We used to use the above locate-dominating-files code, but the
-    ;; directory-files call is very costly, so we're much better off doing
-    ;; multiple calls using the code in here.
-    ;;
-    ;; Represent /home/luser/foo as ~/foo so that we don't try to look for
-    ;; `name' in /home or in /.
-    (setq file (abbreviate-file-name file))
-    (let ((root nil)
-          (prev-file file)
-          ;; `user' is not initialized outside the loop because
-          ;; `file' may not exist, so we may have to walk up part of the
-          ;; hierarchy before we find the "initial UID".
-          (user nil)
-          try)
-      (while (not (or root
-                      (null file)
-                      ;; FIXME: Disabled this heuristic because it is sometimes
-                      ;; inappropriate.
-                      ;; As a heuristic, we stop looking up the hierarchy of
-                      ;; directories as soon as we find a directory belonging
-                      ;; to another user.  This should save us from looking in
-                      ;; things like /net and /afs.  This assumes that all the
-                      ;; files inside a project belong to the same user.
-                      ;; (let ((prev-user user))
-                      ;;   (setq user (nth 2 (file-attributes file)))
-                      ;;   (and prev-user (not (equal user prev-user))))
-                      (string-match locate-dominating-stop-dir-regexp file)))
-        (setq try (file-exists-p (expand-file-name name file)))
-        (cond (try (setq root file))
-              ((equal file (setq prev-file file
-                                 file (file-name-directory
-                                       (directory-file-name file))))
-               (setq file nil))))
-      root))
-
-  (defvar locate-dominating-stop-dir-regexp
-    "\\`\\(?:[\\/][\\/][^\\/]+\\|/\\(?:net\\|afs\\|\\.\\.\\.\\)/\\)\\'"))
 
 (provide 'dominating-file)
 
@@ -593,6 +547,8 @@ $.
   ;; Your init file should contain only one such instance.
   ;; If there is more than one, they won't work right.
  '(Man-width nil t)
+ '(ns-alternate-modifier (quote super))
+ '(ns-command-modifier (quote meta))
  '(safe-local-variable-values (quote ((sh-basic-offset . 3) (encoding . utf-8) (cperl-indent-level . 4) (cperl-indent-level . 2))))
  '(speedbar-show-unknown-files t))
 (custom-set-faces
