5a02eea6097ed9d8a806205eb6a4ff836f28a41b
Florian Ebeling <florian.ebeling@gmail.com>
2012-12-06 14:20:52 +0100

    Customize blob

--- init.el
+++ init.el
@@ -594,3 +594,9 @@ and save it."
 
 (put 'erase-buffer 'disabled nil)
 (put 'upcase-region 'disabled nil)
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
